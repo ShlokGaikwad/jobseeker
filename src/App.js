ac3b0ed262fb8707d0caa11dfd59ee2391c8e5a2
Shlok gaikwad <shlokgaikwad8888@gmail.com>
2023-07-04 13:20:07 +0530

    error page Completetd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import Landing from "./pages/Landing";
-import styled from "styled-components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Landing, Error, Register, Dashboard } from "./pages/index";
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="landing" element={<Landing />} />
+        <Route path="*" element={<Error />} />
+        <Route path="register" element={<Register />} />
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,17 +1,14 @@
 import React from "react";
-import logo from "../assets/images/logo.svg";
+import { Logo } from "../components/index";
 import main from "../assets/images/main.svg";
-import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img
-          src={logo}
-          alt="jobseeker logo"
-          className="logo"
-        />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
@@ -26,55 +23,12 @@ const Landing = () => {
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to='/register' className="btn btn-hero">Login/Register</Link>
         </div>
         <img src={main} alt="mainLogo" className="img main-img" />
       </div>
     </Wrapper>
   );
 };
-
-const Wrapper = styled.main`
-  nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-    font-size: 10rem;
-  }
-
-  .page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -3rem;
-  }
-
-  h1 {
-    font-weight: 700;
-    span {
-      color: var(--primary-color);
-    }
-  }
-  p {
-    color: var(--grey-600);
-  }
-
-  .main-img {
-    display: none;
-  }
-
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: 1fr 1fr;
-      column-gap: 3rem;
-    }
-    .main-img {
-      display: block;
-    }
-  }
-`;
 
 export default Landing;
