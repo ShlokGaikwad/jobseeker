84dbfcef5335a86d12a06c2b472e60bd6789dc96
Shlok gaikwad <shlokgaikwad8888@gmail.com>
2023-07-04 12:12:36 +0530

    landing page done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Landing from "./pages/Landing";
+import styled from "styled-components";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Landing.js
@@ -0,0 +1,80 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import styled from "styled-components";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img
+          src={logo}
+          alt="jobseeker logo"
+          className="logo"
+        />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="mainLogo" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+    font-size: 10rem;
+  }
+
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-color);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+
+  .main-img {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
+
+export default Landing;
