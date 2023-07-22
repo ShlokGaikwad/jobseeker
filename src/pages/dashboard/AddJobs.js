f3e2a9662cdd707d3cfc294f2b1f00ea28d4d41d
Shlok gaikwad <shlokgaikwad8888@gmail.com>
2023-07-22 14:11:52 +0530

    Add job component sturcture setup

--- src/pages/dashboard/AddJobs.js
+++ src/pages/dashboard/AddJobs.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import Wrapper from "./../../assets/wrappers/DashboardFormPage";
+import FormRow from "./../../components/FormRow";
 
 const AddJobs = () => {
-  return (
-    <h1>AddJobs</h1>
-  )
-}
+  const {
+    isLoading,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    isEditing,
+    editJobId,
+  } = useSelector((store) => store.job);
 
-export default AddJobs 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!position || !company || !jobLocation) {
+      toast.error("please fill out all fields");
+      return;
+    }
+  };
+
+  const handleJobInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(name, value);
+  };
+
+  return (
+    <Wrapper>
+      <form className="form">
+        <h3>{isEditing ? "edit job" : "add job"}</h3>
+        <div className="form-center">
+          {/* position */}
+          <FormRow
+            type="text"
+            name="position"
+            value={position}
+            handleChange={handleJobInput}
+          />
+          {/* company */}
+          <FormRow
+            type="text"
+            name="company"
+            value={company}
+            handleChange={handleJobInput}
+          />
+          {/* job location */}
+          <FormRow
+            type="text"
+            name="jobLocation"
+            labelText="job location"
+            value={jobLocation}
+            handleChange={handleJobInput}
+          />
+          <div className="btn-container">
+            <button
+              type="button"
+              className="btn btn-block clear-btn"
+              onClick={() => console.log("clear value")}
+            >
+              clear
+            </button>
+            <button
+              type="submit"
+              className="btn btn-block submit-btn"
+              onClick={handleSubmit}
+              disabled={isLoading}
+            >
+              submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default AddJobs;
